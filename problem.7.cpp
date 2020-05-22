f1e3a337ed36ef9549224b4687031e86fed394d5
tonatiuh pena <penat@tonatiuh.de>
2020-05-22 22:27:51 +0200

    Added Problem 7

---
+++ problem.7.cpp
@@ -0,0 +1,51 @@
+/* 
+PROBLEM 7
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+
+Tonatiuh Pena-Centeno
+21-Mai-2020
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Function that checks whether n is prime or not
+int isPrimeNumber(int n) {
+  bool isPrime = true;
+
+  for(int i = 2; i <= n/2; i++) {
+	if (n%i == 0) {
+	  isPrime = false;
+	  break;
+	}
+  }
+  return isPrime;
+}
+
+int main()
+{
+  int upperLimit = 10001;
+  int n = 2;
+  int iter = 0;
+  bool stop = false;
+  bool verbose = true;
+  
+  while (stop == false & iter < upperLimit) {
+	
+	if (isPrimeNumber(n) == true) {
+	  std::cout << n << " : is prime ";
+	  iter++;
+	  std::cout << " (iter=" << iter << ")" << "\n"; 
+	}
+	
+	n++;
+	
+  }
+
+  
+  return 0;
+}
